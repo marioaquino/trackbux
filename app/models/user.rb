8950119df01889a5e8217fa796f186790337dbb4
Mario Aquino <mario.e.aquino@gmail.com>
2010-01-17 17:59:58 -0600

    Setting the user's time zone from the formatted time zone string should just keep the pretty wordz.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,10 @@ class User
   validates_with_block :time_zone do 
     ActiveSupport::TimeZone.all.find{|zone| zone.name == @time_zone}
   end
-  
 
   has 1, :budget
+  
+  def time_zone=(value)
+    attribute_set(:time_zone, value.split(/\)\s/).last)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,12 @@ describe User do
     @user.time_zone = 'foo'
     @user.valid?.should be_false
   end
+  
+  it "should strip formatted time zones and just store the words" do
+    {"(GMT+07:00) Bangkok" => 'Bangkok', 
+     "(GMT-05:00) Eastern Time (US & Canada)" => "Eastern Time (US & Canada)" }.each_pair do |k, v|
+        @user.time_zone = k
+        @user.time_zone.should == v
+      end
+  end
 end
