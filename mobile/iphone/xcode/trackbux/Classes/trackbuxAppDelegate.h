336fc467834af5a3b5b7a674adb414f260b3f52d
Mario Aquino <mario.e.aquino@gmail.com>
2010-02-13 11:30:01 -0600

    Added Xcode project for trackbux iPhone. Also moved Dashcode project beneath mobile/iphone directory.

---
+++ mobile/iphone/xcode/trackbux/Classes/trackbuxAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  trackbuxAppDelegate.h
+//  trackbux
+//
+//  Created by Mario Aquino on 2/13/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface trackbuxAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
