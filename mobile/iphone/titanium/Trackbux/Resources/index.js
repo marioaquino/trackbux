291ce09d2a84204a312dd8d7eafbccabfb87f78d
Mario Aquino <mario.e.aquino@gmail.com>
2010-02-20 22:30:27 -0600

    Got the gauge ui working in titanium. I also removed some generated build-related files from the repo. Hope this doesn't break something.

--- mobile/iphone/titanium/Trackbux/Resources/index.js
+++ mobile/iphone/titanium/Trackbux/Resources/index.js
@@ -1,9 +1,13 @@
 window.onload = function()
 {
-	document.getElementById('version').innerHTML = Titanium.version;
-	document.getElementById('phone').innerHTML = Titanium.Platform.name + ' '+Titanium.Platform.version;
-	document.getElementById('b').onclick = function() { Titanium.Media.vibrate(); };
+    spec = { "criticalValue": 90, "imageHeight": 188, 
+			"imagePointer": "Images/gauge_needle.png", 
+			"imageWidth": 57, "minValue": 99, "pivotOffsetX": 28, 
+			"pivotOffsetY": 28, "pointerReach": 85, "startAngle": 45, 
+			"stopAngle": 315, "warningValue": 50 };
 
+	var gaugeObject = CreateGauge("gauge", spec);
+	
 	Titanium.Gesture.addEventListener('shake',function(){
 		var alerty = Titanium.UI.createAlertDialog();
 		alerty.setTitle("Not stirred!");
