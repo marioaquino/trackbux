afebbbb7ba70b376a145e4925b74803c1e87221b
Mario Aquino <mario.e.aquino@gmail.com>
2010-02-25 22:26:48 -0600

    OK.  I think I have it fixed up (knowing which generated files to commit to the repo, that is).

---
+++ mobile/iphone/titanium/Trackbux/build/iphone/main.m
@@ -0,0 +1,35 @@
+//
+//  Appcelerator Titanium Mobile
+//  WARNING: this is a generated file and should not be modified
+//
+
+#import <UIKit/UIKit.h>
+#define _QUOTEME(x) #x
+#define STRING(x) _QUOTEME(x)
+
+NSString * const TI_APPLICATION_DEPLOYTYPE = @"development";
+NSString * const TI_APPLICATION_ID = @"com.maramy";
+NSString * const TI_APPLICATION_PUBLISHER = @"not specified";
+NSString * const TI_APPLICATION_URL = @"not specified";
+NSString * const TI_APPLICATION_NAME = @"Trackbux";
+NSString * const TI_APPLICATION_VERSION = @"1.0";
+NSString * const TI_APPLICATION_DESCRIPTION = @"not specified";
+NSString * const TI_APPLICATION_COPYRIGHT = @"not specified";
+NSString * const TI_APPLICATION_GUID = @"4c92e89a-2c95-4a23-8a37-7ad28379e241";
+BOOL TI_APPLICATION_ANALYTICS = true;
+
+int main(int argc, char *argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+#ifdef __LOG__ID__
+	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+	NSString *documentsDirectory = [paths objectAtIndex:0];
+	NSString *logPath = [documentsDirectory stringByAppendingPathComponent:[NSString stringWithFormat:@"%s.log",STRING(__LOG__ID__)]];
+	freopen([logPath cStringUsingEncoding:NSUTF8StringEncoding],"w+",stderr);
+	fprintf(stderr,"[INFO] Application started\n");
+#endif
+
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
